873acee20cae707da56af2fd2f1923768b5af63f
jas75 <azoulayjason@gmail.com>
2017-08-05 18:34:12 +0200

    route setup and app-routin module creation

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
