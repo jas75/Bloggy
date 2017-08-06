ce121c3fd01f4979eae2078fd83203dba2a4bb03
jas75 <azoulayjason@gmail.com>
2017-08-07 00:57:54 +0200

    Finish form Validations

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -19,11 +19,71 @@ export class RegisterComponent implements OnInit {
 
   createForm(){
   	this.form= this.formBuilder.group({
-  		email:'',
-  		username:'',
-  		password:'',
-  		confirm:''
-  	});
+  		email:['', Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(40),
+          this.validateEmail
+        ])],
+  		username:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(30),
+          this.validateUsername
+        ])],
+  		password:['',Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(40),
+          this.validatePassword
+        ])],
+  		confirm:['',Validators.required]
+  	},{ validator: this.matchingPasswords('password','confirm')});
+  }
+
+  validateEmail(controls){
+    const regExp= new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (regExp.test(controls.value)){
+      return null;
+    }
+    else{
+      return { 'validateEmail': true};
+    } 
+  }
+
+  validateUsername(controls){
+    const regExp= new RegExp(/^[a-zA-z0-9]+$/);
+    if (regExp.test(controls.value)){
+      return null;
+    }
+    else{
+      return { 'validateUsername':true};
+    }
+  }
+
+  validatePassword(controls){
+    const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+    if(regExp.test(controls.value)){
+      return null;
+    }
+    else{
+      return { 'validatePassword':true};
+    }
+  }
+
+  matchingPasswords(password,confirm){
+    return (group: FormGroup)=>{
+      if(group.controls[password].value === group.controls[confirm].value){
+        return null;
+      }
+      else{
+        return {'matchingPasswords':true};
+      }
+    }
+  }
+
+  onRegisterSubmit(){
+  	console.log("form submitted");
   }
   ngOnInit() {
   }
