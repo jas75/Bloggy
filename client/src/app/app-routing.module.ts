fa174fb9657160e1c432c6b8b04528c28e9c937b
jas75 <azoulayjason@gmail.com>
2017-08-06 21:06:03 +0200

    Register component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 // Our Array of Angular 2 Routes
@@ -8,6 +9,10 @@ const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent // The Default Route
+  },
+  {
+    path:'register',
+    component: RegisterComponent
   },
   { path: '**', component: HomeComponent } // The "Catch-All" Route
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component'; 
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
