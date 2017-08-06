a7fdec82875b6aa54bf15eb78928e53247ad0119
jas75 <azoulayjason@gmail.com>
2017-08-06 18:48:17 +0200

    First route

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports=
 {
-	uri: 'mongodb://localhost:27017/' +this.db,
+	uri: 'mongodb://localhost:27017/Bloggy',
 	secret: crypto,
 	db: 'Bloggy'
 }
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,37 @@ const User= require('../models/user');
 module.exports= (router)=>{
 
 	router.post('/register',(req,res)=>{
-		res.send('test');
+		
+		if (!req.body.email) {
+			res.json({success:false,message: 'You must provide an e-mail'});
+		}
+		else{
+			if (!req.body.username) {
+				res.json({success:false,message:'You must provide a username'});
+			}
+			else{
+				if(!req.body.password){
+					res.json({success:false,message:'You must provide a password'});
+				}
+				else{
+					let user= new User({
+						email: req.body.email.toLowerCase(),
+						username: req.body.username.toLowerCase(),
+						password: req.body.password
+					});
+
+					user.save((err)=>{
+						if (err) {
+							res.json({success:false,message:'Could not save user :', err})
+						}
+						else{
+							res.json({success:true,message:'Account registered'})
+						}
+					});
+				}
+			}
+			
+		}
 	});
 	return router;
 };
