93f9e8ad157d0bf623e43e155a5832faa24c5633
jas75 <azoulayjason@gmail.com>
2017-08-06 19:03:50 +0200

    Hash password middleware

--- models/user.js
+++ models/user.js
@@ -1,11 +1,22 @@
 const mongoose=require('mongoose');
 mongoose.Promise = global.Promise;
 const Schema= mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
-const userSchema={
+const userSchema=new Schema({
 	email: { type: String, required: true, unique: true, lowercase: true},
 	username: { type: String, required: true, unique: true, lowercase: true},
 	password: { type: String, required: true}
-}
+});
+// Middleware that encrypte password 
+userSchema.pre('save', function(next){
+	if(!this.isModified('password'))
+	return next();
 
+	bcrypt.hash(this.password, null, null, (err,hash)=>{
+		if(err) return next(err);
+		this.password=hash;
+		next();
+	});
+});
 module.exports = mongoose.model('User',userSchema);
