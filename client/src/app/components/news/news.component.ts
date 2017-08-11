b5f9b24d330c2b60ec0f5ee41efec972508c50d6
jas75 <azoulayjason@gmail.com>
2017-08-11 16:09:37 +0200

    Styles Posts

--- client/src/app/components/news/news.component.ts
+++ client/src/app/components/news/news.component.ts
@@ -9,7 +9,19 @@ export class NewsComponent implements OnInit {
 
 
 	isDisabled:boolean=true;
+	message;
+	messageClass;
+	loadingPosts=false;
+
   constructor() { }
+
+  reloadPosts(){
+  	this.loadingPosts=true;
+  	//Get all blogs
+  	setTimeout(()=>{
+  		this.loadingPosts=false;
+  	},3000);
+  }
 
   ngOnInit() {
   }
